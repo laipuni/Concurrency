ac8caaf327f689594b3813349c71a1dd6e1ca982
laipuni <2tgb02023@gmail.com>
2024-05-15 12:24:30 +0900

    error : remove @Profile("test") in ControllerIntegrationTest because of UnsatisfiedDependencyException

--- src/test/java/com/example/shopproject/controller/ControllerIntegrationTest.java
+++ src/test/java/com/example/shopproject/controller/ControllerIntegrationTest.java
@@ -5,8 +5,10 @@ import com.example.shopproject.controller.order.OrderController;
 import com.example.shopproject.domain.item.ItemService;
 import com.example.shopproject.domain.order.OrderFacade;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,7 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.web.servlet.MockMvc;
 
-@Profile("test")
+
 @WebMvcTest(controllers = {
         ItemController.class,
         OrderController.class
